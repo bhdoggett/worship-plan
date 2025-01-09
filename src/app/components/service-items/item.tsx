33aa4651edc97563eae8453c969d429f72994727
Ben Doggett <bdoggett@gmail.com>
2025-01-09 14:35:54 -0500

    Add width styling to service item.

--- src/app/components/service-items/item.tsx
+++ src/app/components/service-items/item.tsx
@@ -6,16 +6,16 @@ const Item = ({ time, title, description, person }) => {
       className="flex items-start gap-32
      items-center border"
     >
-      <div className="duration text-sm text-gray-600">
+      <div className="duration text-sm text-gray-600 w-10">
         {time.minutes}:{time.seconds}
       </div>
-      <div className="title-description flex flex-col">
+      <div className="title-description flex flex-col w-20">
         <div className="title font-bold text-lg">{title}</div>
         <div className="description whitespace-pre-wrap text-gray-800">
           {description}
         </div>
       </div>
-      <div className="person text-sm text-gray-500">{person}</div>
+      <div className="person text-sm text-gray-500 w-10">{person}</div>
     </div>
   );
 };
